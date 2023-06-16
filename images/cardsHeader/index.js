04a0c533693c60ab07b97dd7bb7057a520b90970
jackson <jackson97parada@gmail.com>
2023-06-16 17:01:56 -0500

    Updates

---
+++ images/cardsHeader/index.js
@@ -0,0 +1,3 @@
+export { default as firstCard } from "./firstcardheader.png";
+export { default as secondCard } from "./secondcardheader.png";
+export { default as threeCard } from "./threecardheader.png";
---
+++ images/index.js
@@ -0,0 +1,1 @@
+export { default as imageHeader } from "./image-header.png";
